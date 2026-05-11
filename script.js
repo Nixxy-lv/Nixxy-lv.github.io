c001729b60e7515a0c9e81eaf791e74a5fd9c5b5
Nixxy-lv <diegodimeo9@gmail.com>
2026-05-11 19:27:55 +0200

    page redesign

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-const toggle = document.getElementById("themeToggle");
 
-toggle.addEventListener("change", () => {
-  document.body.classList.toggle("light");
-
-  localStorage.setItem(
-    "theme",
-    document.body.classList.contains("light") ? "light" : "dark"
-  );
-});
-
-// restore
-if (localStorage.getItem("theme") === "light") {
-  document.body.classList.add("light");
-  toggle.checked = true;
+// for future
+if (localStorage.getItem("theme") === "light") {  
+  document.body.classList.add("light");  
 }
